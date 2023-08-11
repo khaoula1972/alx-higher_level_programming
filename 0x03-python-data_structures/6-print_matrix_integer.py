af8429de5020982e0f42f968f96e5751e7177480
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-11 06:05:08 +0100

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    C = len(matrix[0])
+    L = len(matrix)
+    for i in range(L):
+        for j in range(C):
+            e = " "
+            if j == C - 1:
+                e = "\n"
+            print("{:d}".format(matrix[i][j]), end=e)
