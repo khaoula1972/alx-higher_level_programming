4685f92cb8f817ea7c44dd9fbe625f69e8fef5f1
Khaoula Nakkach <123691690+khaoula1972@users.noreply.github.com>
2023-08-14 23:54:57 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None or element is None:
-        L = my_list.copy()
-    if not (idx < 0 or idx >= len(my_list)):
-        L = my_list.copy()
-        L[idx] = element
-    return (L)
+    L = my_list.copy()
+    for x in my_list:
+        if idx == my_list.index(x):
+            L[idx] = element
+            return (L)
+        elif idx >= len(my_list):
+            return (L)
+        elif idx < 0:
+            return (L)
