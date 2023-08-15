4809032c70ecfcd5e1e600d1bd92d457b252de7d
Khaoula Nakkach <123691690+khaoula1972@users.noreply.github.com>
2023-08-15 03:18:03 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
     L = len(matrix)
+    if L == 0:
+        print("{}".format("[[]]"))
     for i in range(L):
         C = len(matrix[i])
         for j in range(C):
