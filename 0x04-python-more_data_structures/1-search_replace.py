ee2c6c78aa482ecd30157a350a30834519d1f510
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-16 16:16:14 -0700

    search and replace value in list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if search in my_list:
-        L = [replace if i == search else i for i in my_list.copy()]
-        return(L)
+    L = [replace if i == search else i for i in my_list.copy()]
+    return(L)
