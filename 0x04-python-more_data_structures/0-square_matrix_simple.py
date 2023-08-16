a1be40b7ff60ec87648396350684231e39e9cdcd
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-16 15:45:59 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,6 @@ def square_matrix_simple(matrix=[]):
     new = list()
     L = len(matrix)
     C = len(matrix[0])
-    if L == 0:
-        return None
     for i in range(L):
         k = list()
         for j in range(C):
