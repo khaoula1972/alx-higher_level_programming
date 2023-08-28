9c265c713de64612d7f42aff8de88f5d75d57dd2
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-28 22:14:02 +0100

    safely print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    l = 0;
+    length = 0
     L = my_list.copy()
     while (L):
-        l += 1
+        length += 1
         L.pop()
     try:
-        for i in range (x):
+        for i in range(x):
             print(my_list[i], end="")
         print()
         return (x)
     except IndexError:
         print()
-        return (l)
+        return (length)
