ad873e2ee90517075bbed1e6c2f2920f23993298
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-29 23:09:32 +0100

    Print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+A class: Square
+
+A square is a four-sided polygon with all side of equal
+length and all angles right angles.
+"""
+
+
+class Square:
+    """
+    This is Square class
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a square with the given side length.
+
+        Args:
+            size: The length of all sides of the square.
+            type: positive integer
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieve the size of the square.
+
+        Returns:
+            int: The length of all sides of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The length of all sides of the square.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, (int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Compute the area of a square with a given side length.
+
+        Args:
+            __size: the length of the side
+
+        Returns:
+                area of the square
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        Print in stdout the square with the character #.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size + 1):
+                if i != self.__size - 1:
+                    print("#" * self.__size)
