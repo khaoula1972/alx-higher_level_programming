75bf5be67cab125563406ddc16d52aae31febe0e
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-29 21:37:39 +0100

    Class Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+An empty class called Square
+"""
+
 class Square:
     """
     This class represents a square.
 
     A square is a four-sided polygon with all side of equal
     length and all angles right angles.
+
+    This is an empty class though.
     """
     pass
