6746cb6779aa5c55cecec55d7e968eac39c1015a
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-29 23:56:56 +0100

    Postional square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,10 +71,10 @@ class Square:
         Raises:
             TypeError: If the value is not an integer.
         """
+        message = "position must be a tuple of 2 positive integers"
         if not isinstance(value, tuple) or len(value) != 2 or \
                 not all(isinstance(val, int) and val >= 0 for val in value):
-            raise TypeError("position \
-                    must be a tuple of 2 positive integers")
+            raise TypeError(message)
         else:
             self.__position = value
 
