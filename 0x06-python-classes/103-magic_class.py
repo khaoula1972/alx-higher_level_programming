a8e5276fb17e9735d8dfd1651bf5f392918b43f6
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-30 01:32:21 +0100

    Area and perimeter of cicumference

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,18 +4,19 @@ import math
 
 class MagicClass:
     """
-    This class calculate the area and the perimeter of
-    a circumference."""
+    This class calculate the area and the perimeter of a circumantance
+    """
+
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
-    """ This method calculate the area. """
     def area(self):
+        """ This method calculate the area. """
         return ((self.__radius ** 2) * math.pi)
 
-    """ This method calculates the perimeter """
     def circumference(self):
+        """ This method calculates the perimeter """
         return (2 * math.pi * self.__radius)
