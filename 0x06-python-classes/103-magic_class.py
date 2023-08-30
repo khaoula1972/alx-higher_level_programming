742bc7c76d685924b04525c42702c67ac191539c
khaoula1972 <khaoula.nakkach@gmail.com>
2023-08-30 01:56:43 +0100

    Area and perimeter of cicumference

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -21,7 +21,7 @@ class MagicClass:
 	    radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
