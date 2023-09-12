b42293c1ed749108bcb24d9c6d9497ea465b6bd6
khaoula1972 <khaoula.nakkach@gmail.com>
2023-09-12 02:33:33 +0100

    validating value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
         """
         This method validates value
         """
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
