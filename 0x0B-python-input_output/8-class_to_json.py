d590c8fd3f1c42f1c078adbe1d8cd176313662ec
khaoula1972 <khaoula.nakkach@gmail.com>
2023-09-17 15:12:39 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a module conatining a function
+"""
+
+
+def class_to_json(obj):
+    """
+    a function that returns the dictionary description
+    """
+
+    if isinstance(obj, (int, str, bool)):
+        return obj
+    elif isinstance(obj, list):
+        return [class_to_json(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif hasattr(obj, '__dict__'):
+        return {key: class_to_json(value) for key, value in obj.__dict__.items()}
+    else:
+        return str(obj)  # Handle other types as strings
