19ef09d72756b589c81301e96b16ca354bd0bac0
khaoula1972 <khaoula.nakkach@gmail.com>
2023-09-17 14:21:06 +0100

    Add item in file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains a script that loads, adds, and saves files
+"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    try:
+        existing_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        existing_list = []
+
+    if len(sys.argv) > 2:
+        args = sys.argv[1:]
+        existing_list.extend(args)
+
+    save_to_json_file(existing_list, "add_item.json")
