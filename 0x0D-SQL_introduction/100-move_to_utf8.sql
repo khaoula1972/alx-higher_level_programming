5e7d043c9371d25fdbd2acf7ec522ec6a82d673c
khaoula1972 <khaoula.nakkach@gmail.com>
2023-10-18 03:14:03 +0100

    converts to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,12 @@
 -- Go to UTF8
 
+USE hbtn_0c_0;
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 -- Next, convert the table character set and collation
-ALTER TABLE hbtn_0c_0.first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
--- Finally, convert the field (name) character set and collation within the first_table
-ALTER TABLE hbtn_0c_0.first_table
-    MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
