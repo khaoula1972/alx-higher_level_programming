231ab5bf3a330801bb44cdf47544436f41db0691
khaoula1972 <khaoula.nakkach@gmail.com>
2023-10-19 01:50:34 +0100

    List the priviliges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- My privileges!
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
+-- List privileges for user_0d_1
+SELECT * FROM user WHERE user='user_0d_1';
+-- List privileges for user_0d_2
+SELECT * FROM user WHERE user='user_0d_2';
