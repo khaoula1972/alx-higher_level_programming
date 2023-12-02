91181bc58ec4107b7b493c3ec6f661aab99a8475
khaoula1972 <khaoula.nakkach@gmail.com>
2023-12-02 01:54:35 +0100

    My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Script that takes GitHub Credentials (username and password)
+    used GitHub API to display your id
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    r_js = r.json()
+    if r_js == {}:
+        print("None")
+    else:
+        print("{}".format(r_js.get('id')))
